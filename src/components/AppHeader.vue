22cfa20761b55960322d76757cee9af0cad67fa3
snerva <selenenerva@live.it>
2023-01-31 09:21:30 +0100

    some style

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -8,39 +8,38 @@ export default {
 <template>
     <a name="back"></a>
     <header>
-        <nav class="navbar navbar-dark navbar-expand-md">
-            <div class="container-fluid">
-                <router-link class="navbar-brand p-2" :to="{ name: 'home' }" aria-current="page">
-                    <img class="img-fluid logo" src="/images/logo.png" alt="">
-                </router-link>
-                <button class="navbar-toggler collapsed d-lg-none" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+        <div class="nav-menu container d-flex flex-wrap align-items-center justify-content-between p-3">
+            <router-link class="brand p-2" :to="{ name: 'home' }" aria-current="page">
+                <img class="img-fluid logo" src="/images/logo.png" alt="">
+            </router-link>
+            <div class="menu-toggler d-lg-none">
+                <button class="toggler">
+                    <span class="toggler-icon"></span>
                 </button>
-                <div class="navbar-collapse collapse" id="navbarSupportedContent" style>
-                    <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
-                        <li class="nav-item">
-                            <router-link active-class="active" class="nav-link" :to="{ name: 'home' }"
-                                aria-current="page">home
-                                <span class="visually-hidden">(current)</span></router-link>
-                        </li>
-                        <li class="nav-item">
-                            <router-link active-class="active" class="nav-link"
-                                :to="{ name: 'about' }">about</router-link>
-                        </li>
-                        <li class="nav-item">
-                            <router-link active-class="active" class="nav-link"
-                                :to="{ name: 'projects' }">projects</router-link>
-                        </li>
-                        <li class="nav-item">
-                            <router-link active-class="active" class="nav-link"
-                                :to="{ name: 'contact' }">contact</router-link>
-                        </li>
-                    </ul>
-                </div>
             </div>
-        </nav>
+
+            <div class="menu">
+                <ul class="d-flex list-unstyled gap-4 ms-auto mt-2 mt-lg-0">
+                    <li class="nav-item">
+                        <router-link active-class="active" class="nav-link" :to="{ name: 'home' }"
+                            aria-current="page">home
+                            <span class="visually-hidden">(current)</span></router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link active-class="active" class="nav-link" :to="{ name: 'about' }">about</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link active-class="active" class="nav-link"
+                            :to="{ name: 'projects' }">projects</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link active-class="active" class="nav-link"
+                            :to="{ name: 'contact' }">contact</router-link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
 
     </header>
 </template>
